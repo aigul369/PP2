f6bd173ec8791c526f0996ef5f21dd5028c72822
aigul369 <a_nabi@kbtu.kz>
2026-01-29 19:37:27 +0500

    add syntax example

---
+++ practice01/syntax.py
@@ -0,0 +1,27 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#3
+for i in range(1, 6):
+    print("Number:", i)
+
+#4
+count = 1
+
+while count <= 5:
+    print("Count:", count)
+    count += 1
+
+#5
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+

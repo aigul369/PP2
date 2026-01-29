4160649bc67b039d5deee01b5e5234ba330d4023
aigul369 <a_nabi@kbtu.kz>
2026-01-29 20:09:07 +0500

    add casting example

---
+++ practice01/casting.py
@@ -0,0 +1,29 @@
+#1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+#4
+price_str = "19.99"
+price = float(price_str)
+
+print(price)
+print(type(price))
+
+#5
+year = 2025
+text = str(year)
+
+print("Year is " + text)
+print(type(text))

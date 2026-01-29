978a05fb9b0f18db7496563ffda18f457ba54001
aigul369 <a_nabi@kbtu.kz>
2026-01-29 19:07:22 +0500

    add strings example

--- practice01/strings.py
+++ practice01/strings.py
@@ -1,2 +1,19 @@
 #1 
 print("HELLO!")
+
+#2
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#3
+a = "Hello, World!"
+print(len(a))
+
+#4
+a = "Hello, World!"
+print(a[1])
+
+#5
+txt = "The best things in life are free!"
+print("free" in txt)

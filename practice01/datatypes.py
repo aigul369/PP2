b9792b9c7bac38acadf9f49b64e536e0b2c8088e
aigul369 <a_nabi@kbtu.kz>
2026-01-29 19:30:13 +0500

    add datatypes example

---
+++ practice01/datatypes.py
@@ -0,0 +1,34 @@
+#1
+x = 5
+print(type(x))
+
+#2
+student = {
+    "name": "Aigul",
+    "age": 20,
+    "is_student": True
+}
+
+print(student)
+print(type(student))
+
+#3
+price = 19.99
+height = 1.65
+
+print(price)
+print(type(price))
+
+
+#4
+unique_numbers = {1, 2, 3, 3, 4}
+
+print(unique_numbers)
+print(type(unique_numbers))
+
+#5
+is_student = True
+has_job = False
+
+print(is_student)
+print(type(is_student))

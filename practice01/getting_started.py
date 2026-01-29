3f87ab7f1cda9d21e506f1f8f8ac0a1a80f07dd5
aigul369 <a_nabi@kbtu.kz>
2026-01-29 20:13:56 +0500

    add getting started example

---
+++ practice01/getting_started.py
@@ -0,0 +1,22 @@
+#1
+print("Hello, Python!")
+
+#2
+name = "Aigul"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+#3
+import sys
+
+print(sys.version)
+
+#4
+print("2 + 3 =", 2 + 3)
+print("10 * 4 =", 10 * 4)
+print("5 ** 2 =", 5 ** 2)
+
+#5
+print("Hello" * 3)

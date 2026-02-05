6126b397c55a2a30659c43be9b7ce16224e515d3
aigul369 <a_nabi@kbtu.kz>
2026-02-06 00:05:54 +0500

    add Boolean Operators example

---
+++ practice02/boolean/boolean_operators.py
@@ -0,0 +1,19 @@
+#1
+print(5 and 10)
+print(0 and 10)
+
+#2
+print("" or "Python")
+print(0 or 100)
+
+#3
+x = 0
+print(x != 0 and 10 / x)
+
+#4
+print(not [])
+print(not "hello")
+
+#5
+result = "Hello" and [] or "World"
+print(result)

9526b651aadb4f440b2d549c2c040430624e6cbc
aigul369 <a_nabi@kbtu.kz>
2026-02-06 00:02:38 +0500

    add Booleans as Comparison Results example

---
+++ practice02/boolean/boolean_comparison.py
@@ -0,0 +1,29 @@
+#1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#2
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#3
+x = 200
+print(isinstance(x, int))
+
+#4
+print("apple" < "banana")
+print("Zoo" < "apple")
+
+#5
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b)
+print(a is b)
+

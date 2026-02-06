a29b0c6420a14bd6d13b37a2769551e8239d6253
aigul369 <a_nabi@kbtu.kz>
2026-02-06 18:16:48 +0500

    add for loops example

---
+++ practice02/loops/for_loop.py
@@ -0,0 +1,29 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#2
+for x in "banana":
+  print(x)
+
+#3
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#4
+total = 0
+for i in range(1, 6):
+    total += i
+
+print(total)
+
+#5
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")

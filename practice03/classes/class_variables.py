c511390362f33016de1834fbafd7aa0b5a4da08b
aigul369 <a_nabi@kbtu.kz>
2026-02-14 00:56:07 +0500

    add class variables examples

---
+++ practice03/classes/class_variables.py
@@ -0,0 +1,59 @@
+#1
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#2
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
+
+#3
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Tobias", 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age)
+
+#4
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Tobias", 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age)
+
+#5
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+p1 = Person("Tobias")
+
+p1.age = 25
+p1.city = "Oslo"
+
+print(p1.name)
+print(p1.age)
+print(p1.city)

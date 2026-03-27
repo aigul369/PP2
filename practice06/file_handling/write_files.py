8ca59aba8f85f0862fb29baf40dbc9027e799233
aigul369 <a_nabi@kbtu.kz>
2026-03-27 15:14:05 +0500

    Add Practice6 - file handling write examples

---
+++ practice06/file_handling/write_files.py
@@ -0,0 +1,28 @@
+"""
+"a" - Append - will append to the end of the file
+
+"w" - Write - will overwrite any existing content
+
+"""
+#EX1 если файл не существует, то создаст новый
+with open(r"test.txt", "a") as f:
+    f.write("Now the file has more content!\n")
+
+with open(r"test.txt") as f:
+    print(f.read())
+
+
+#EX2 Если не существует, то создается новый и записывается
+with open(r"test.txt", "w") as f:
+    f.write("the content\n")
+
+with open(r"test.txt") as f:
+    print(f.read())
+
+#EX 3 проверка 
+try:
+    with open(r"newfile.txt", "x") as f:
+        f.write("This is a new file")
+    print("newfile.txt")
+except FileExistsError:
+    print("file already exists")
